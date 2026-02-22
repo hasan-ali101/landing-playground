c1d7eecce4f17d4ae98db2563ecd76991eddcd61
Hasan Ali <hasan15.06.96@gmail.com>
2026-02-22 18:34:03 +0000

    github ui

---
+++ app/components/helmet-canvas.tsx
@@ -0,0 +1,105 @@
+"use client";
+
+import { Model } from "@/app/components/helmet";
+import { Sparkles, SpotLight } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import { useEffect, useRef, useState } from "react";
+
+export const HelmetCanvas = () => {
+  const [isHovered, setIsHovered] = useState(false);
+  const [isInView, setIsInView] = useState(false);
+  const [globalPointer, setGlobalPointer] = useState({ x: 0, y: 0 });
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        setIsInView(entry.isIntersecting);
+      },
+      {
+        threshold: 0.2, // Percent of the element that needs to be visible to trigger
+        rootMargin: "200px", // Start loading slightly before it enters viewport (200px margin)
+      },
+    );
+
+    if (containerRef.current) {
+      observer.observe(containerRef.current);
+    }
+
+    return () => {
+      if (containerRef.current) {
+        observer.unobserve(containerRef.current);
+      }
+    };
+  }, []);
+
+  useEffect(() => {
+    const handleMouseMove = (e: MouseEvent) => {
+      if (!containerRef.current) return;
+      const rect = containerRef.current.getBoundingClientRect();
+
+      // Normalize to -1 to 1 range relative to container
+      const x = ((e.clientX - rect.left) / rect.width) * 2 - 1;
+      const y = -((e.clientY - rect.top) / rect.height) * 2 + 1;
+
+      setGlobalPointer({ x, y });
+    };
+
+    const container = containerRef.current;
+    if (container) {
+      container.addEventListener("mousemove", handleMouseMove);
+    }
+
+    return () => {
+      if (container) {
+        container.removeEventListener("mousemove", handleMouseMove);
+      }
+    };
+  }, []);
+
+  return (
+    <div
+      className="w-full h-full overflow-clip grid grid-cols-1 lg:grid-cols-2 px-6"
+      onPointerOver={() => setIsHovered(true)}
+      onPointerOut={() => setIsHovered(false)}
+      ref={containerRef}
+    >
+      <div className="w-full h-100 md:h-140 col-span-1">
+        {isInView && (
+          <Canvas
+            className="w-full h-full"
+            dpr={[1, 2]} // Limit pixel ratio for better performance
+          >
+            <ambientLight intensity={5} />
+            <directionalLight position={[15, -5, 2]} intensity={4} />
+            <directionalLight position={[-15, -5, 2]} intensity={4} />
+            <Model
+              position={[0, -2, -5]}
+              scale={1.5}
+              isHovered={isHovered}
+              globalPointer={globalPointer}
+            />
+            <SpotLight
+              position={[0, -2.5, 0]}
+              distance={10}
+              radiusBottom={6}
+              radiusTop={1}
+              anglePower={7}
+              scale={1.5}
+            />
+            <Sparkles count={100} position={[0, 0, -2]} scale={10} />
+          </Canvas>
+        )}
+      </div>
+      <div className="border-t p-20 border-x rounded-t-lg border-white/20 h-100 md:h-150 bg-linear-to-b from-purple-200/10 to-blue-300/30 from-50% flex items-center justify-center relative">
+        <div className="absolute inset-1 bg-linear-to-b from-purple-400/5 to-indigo-400/30 rounded-3xl blur-md"></div>
+        <div className="relative w-full h-full rounded-lg">
+          <div className="w-full h-full bg-white/80 flex flex-col justify-center rounded-lg items-center z-20 absolute">
+            <p className="text-black">& Glowing Card</p>
+          </div>
+          <div className="absolute bg-indigo-300 blur-lg z-10 -inset-3" />
+        </div>
+      </div>
+    </div>
+  );
+};
--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,81 @@
-import Image from "next/image";
+import { HelmetCanvas } from "./components/helmet-canvas";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
+    <div className="flex min-h-screen py-20 flex-col gap-20 items-center font-sans bg-black">
+      <section className="flex flex-col gap-10 py-16 px-6 md:px-10 ">
+        <h1 className="font-bold text-4xl">Open AI News Grid</h1>
+        <div className="w-fit min-w-100 h-40 border border-white/20 rounded-md p-6 flex flex-col  text-left gap-4">
+          <p>All you need to make this work:</p>
+          <ul className="list-disc list-inside">
+            <li>sticky</li>
+            <li>self-start</li>
+            <li>top</li>
+          </ul>{" "}
+        </div>
+        <div className="grid grid-cols-1 lg:grid-cols-4 w-full h-full gap-20 lg:gap-4">
+          <div className="col-span-1 lg:col-span-3  lg:sticky lg:top-10 self-start font-medium flex flex-col gap-4">
+            <div className="md:aspect-video rounded-lg aspect-4/5 bg-white"></div>
+            <p className="text-4xl">News Item 0</p>
+            <div className="flex gap-4 text-sm">
+              <p>Category</p>
+              <p className="text-neutral-400 font-normal">5 min read</p>
+            </div>
+          </div>
+
+          <div className="grid sm:grid-cols-3 font-medium grid-cols-1 w-full lg:col-span-1 lg:gap-12 lg:grid-cols-1 gap-20 sm:gap-4">
+            <div className="col-span-1 flex flex-col gap-2">
+              <div className="bg-indigo-300 rounded-lg  aspect-square w-full"></div>
+              <p className="text-lg">News Item 1</p>
+              <div className="flex gap-4 text-sm">
+                <p>Category</p>
+                <p className="text-neutral-400 font-normal">5 min read</p>
+              </div>
+            </div>
+            <div className="col-span-1 flex flex-col gap-2">
+              <div className="bg-blue-300 rounded-lg aspect-square w-full"></div>
+              <p className="text-lg">News Item 2</p>
+              <div className="flex gap-4 text-sm">
+                <p>Category</p>
+                <p className="text-neutral-400 font-normal">3 min read</p>
+              </div>
+            </div>
+            <div className="col-span-1 flex flex-col gap-2">
+              <div className="bg-amber-100 rounded-lg aspect-square w-full"></div>
+              <p className="text-lg">News Item 3</p>
+              <div className="flex gap-4 text-sm">
+                <p>Category</p>
+                <p className="text-neutral-400 font-normal">8 min read</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="w-fit min-w-100 h-fit border border-white/20 rounded-md p-6 flex flex-col text-left gap-8">
+          <p>
+            The element has position sticky which means that it anchors itself
+            to the viewport at a fixed offset (i.e. top-10) while its container
+            continues to move.
+          </p>
+          <p>
+            A sticky element is structurally bound to its parent, and the
+            container's total height is dictated by the tallest column. Once the
+            bottom of the right-hand column reaches the bottom of the stuck
+            left-hand item, the container 'grabs' the stuck item and pulls it
+            away.
+          </p>
+          <p>
+            Self start (align-self: flex-start) prevents the sticky item from
+            stretching to the height of the tallest column.
           </p>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+      </section>
+
+      <section className="w-full flex flex-col gap-10 pt-16 border border-white/10">
+        <h1 className="font-bold text-4xl text-center -mb-20 lg:mb-0">
+          Github Floating 3d Head
+        </h1>
+        <HelmetCanvas />
+      </section>
     </div>
   );
 }
